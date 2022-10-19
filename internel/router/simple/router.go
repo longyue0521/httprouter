a29e109a7f1df5bbebb5e5539875741e909ebd34
longyue0521 <longyueli0521@gmail.com>
2022-10-19 23:55:51 +0800

    绿灯：通过路径相关测试

--- internel/router/simple/router.go
+++ internel/router/simple/router.go
@@ -1,6 +1,11 @@
 package simple
 
-import "github.com/longyue0521/httprouter/web"
+import (
+	"log"
+	"strings"
+
+	"github.com/longyue0521/httprouter/web"
+)
 
 type Router struct {
 }
@@ -37,8 +42,45 @@ func (p *PathParams) Set(key string, val string) {
 // - 同名路径参数，在路由匹配的时候，值会被覆盖。例如 /user/:id/abc/:id，那么 /user/123/abc/456 最终 id = 456
 func (r *Router) Add(method string, path string, handleFunc web.HandleFunc) {
 
+	subpaths := r.checkPath(path)
+	log.Println(subpaths)
+
 }
 
 func (r *Router) Get(method string, path string) (params PathParams, handleFunc web.HandleFunc, found bool) {
+
+	subpaths := r.checkPath(path)
+	log.Println(subpaths)
+
 	return
+}
+
+func (r *Router) checkPath(path string) []string {
+
+	subpaths := []string{"/"}
+
+	if path == "/" {
+		return subpaths
+	}
+
+	if path == "" {
+		panic("web: 路由不能为空")
+	}
+
+	if path[0] != '/' {
+		panic("web: 路由必须以'/'开头")
+	}
+
+	if path != "/" && path[len(path)-1] == '/' {
+		panic("web: 路由不能以'/'结尾")
+	}
+
+	for _, subpath := range strings.Split(path[1:], "/") {
+		if subpath == "" {
+			panic("web: 路由不能有重复'/'")
+		}
+		log.Println(subpath)
+		subpaths = append(subpaths, subpath)
+	}
+	return subpaths
 }
--- internel/router/simple/router_test.go
+++ internel/router/simple/router_test.go
@@ -22,8 +22,25 @@ func TestRouter(t *testing.T) {
 
 		// case /
 		t.Run("路径", func(t *testing.T) {
+			t.Parallel()
+
+			t.Run("不能为空", func(t *testing.T) {
+				t.Parallel()
+
+				r := NewRouter()
+
+				assert.PanicsWithValue(t, "web: 路由不能为空", func() {
+					r.Add(http.MethodGet, "", nil)
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能为空", func() {
+					r.Get(http.MethodGet, "")
+				})
+			})
 
 			t.Run("必须以'/'开头", func(t *testing.T) {
+				t.Parallel()
+
 				r := NewRouter()
 
 				assert.PanicsWithValue(t, "web: 路由必须以'/'开头", func() {
@@ -31,18 +48,70 @@ func TestRouter(t *testing.T) {
 				})
 
 				assert.PanicsWithValue(t, "web: 路由必须以'/'开头", func() {
-					r.Get(http.MethodGet, "/")
+					r.Get(http.MethodGet, "b")
 				})
 
 			})
 
 			t.Run("不能以'/'结尾", func(t *testing.T) {
+				t.Parallel()
 
+				r := NewRouter()
+
+				assert.NotPanics(t, func() {
+					r.Add(http.MethodGet, "/", handler)
+					r.Get(http.MethodGet, "/")
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能以'/'结尾", func() {
+					r.Add(http.MethodGet, "/a/", handler)
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能以'/'结尾", func() {
+					r.Get(http.MethodGet, "/a/b/")
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能以'/'结尾", func() {
+					r.Add(http.MethodGet, "//", handler)
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能以'/'结尾", func() {
+					r.Get(http.MethodGet, "//")
+				})
 			})
 
-			t.Run("中间无重复'/'", func(t *testing.T) {
+			t.Run("不能有重复'/'", func(t *testing.T) {
+				t.Parallel()
 
+				r := NewRouter()
+
+				assert.PanicsWithValue(t, "web: 路由不能有重复'/'", func() {
+					r.Add(http.MethodGet, "///a//b///c//////d", handler)
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能有重复'/'", func() {
+					r.Get(http.MethodGet, "///a//b///c//////d")
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能有重复'/'", func() {
+					r.Add(http.MethodGet, "//a", handler)
+				})
+
+				assert.PanicsWithValue(t, "web: 路由不能有重复'/'", func() {
+					r.Get(http.MethodGet, "//a")
+				})
 			})
+
+			// t.Run("不能包含空白字符", func(t *testing.T) {
+			// 	t.Parallel()
+			//
+			// 	r := NewRouter()
+			//
+			// 	assert.PanicsWithValue(t, "web: 路由不能包含空白字符", func() {
+			// 		r.Add(http.MethodGet, "\t", nil)
+			// 	})
+			//
+			// })
 		})
 
 		// /user/add/1
